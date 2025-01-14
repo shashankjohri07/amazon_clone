59b10a97151795799c39f3994eded929aeb3c4e8
Shashank <shashankjohri07@gmail.com>
2025-01-14 17:29:03 +0530

    carousel effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./Component/Header";
 import Footer from "./Component/Footer";
 import Center from "./Component/Center";
+import Carousel from "./Component/Carousel";
 
 function App() {
   return (
     <>
       <Header />
+      <Carousel/>
       <Center/>
       <Footer />
     </>
---
+++ src/Component/Carousel.jsx
@@ -0,0 +1,93 @@
+import { useState } from "react";
+
+const Carousel = () => {
+  const [activeIndex, setActiveIndex] = useState(1);
+  const images = [
+    "https://shorturl.at/w6KSH",
+    "https://shorturl.at/6vJLn",
+    "https://shorturl.at/p01ud",
+    "https://shorturl.at/navcS",
+    "https://shorturl.at/fNuH7",
+  ];
+
+  const prevSlide = () => {
+    setActiveIndex((prev) => (prev === 0 ? images.length - 1 : prev - 1));
+  };
+
+  const nextSlide = () => {
+    setActiveIndex((prev) => (prev === images.length - 1 ? 0 : prev + 1));
+  };
+
+  return (
+    <div>
+      <div className="relative w-full">
+        <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
+          {images.map((image, index) => (
+            <div
+              key={index}
+              className={`${
+                activeIndex === index ? "block" : "hidden"
+              } duration-700 ease-in-out`}
+            >
+              <img
+                src={image}
+                className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                alt={`carousel-item-${index + 1}`}
+              />
+            </div>
+          ))}
+        </div>
+        <button
+          type="button"
+          className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+          onClick={prevSlide}
+        >
+          <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+            <svg
+              className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 6 10"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M5 1 1 5l4 4"
+              />
+            </svg>
+            <span className="sr-only">Previous</span>
+          </span>
+        </button>
+        <button
+          type="button"
+          className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+          onClick={nextSlide}
+        >
+          <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+            <svg
+              className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 6 10"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="m1 9 4-4-4-4"
+              />
+            </svg>
+            <span className="sr-only">Next</span>
+          </span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
