3b5bf4b4c46ad60756018430ef0ae87e2c95d565
Shashank <shashankjohri07@gmail.com>
2025-01-16 18:58:37 +0530

    offer card

--- src/Component/OfferCards.jsx
+++ src/Component/OfferCards.jsx
@@ -38,7 +38,7 @@ const OfferCards = () => {
   ];
 
   return (
-    <div className="grid gap-6 p-6 bg-gray-100 sm:grid-cols-1 lg:grid-cols-2">
+    <div className="grid grid-cols-4 gap-6 p-6 bg-gray-100">
       {offers.map((offer, index) => (
         <div className="bg-white shadow-md rounded-lg p-4" key={index}>
           <h3 className="text-lg font-bold mb-4">{offer.title}</h3>
